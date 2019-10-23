30217f6b5cfa73cfeba17b83430d9db09a1d997c
Huan Suh <huansuh@gmail.com>
2019-10-23 19:46:10 +0900

    + Sample dummy UI (StatefulWidget)

--- lib/view/page_main.dart
+++ lib/view/page_main.dart
@@ -2,23 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:todo/model/todo_item.dart';
 import 'package:todo/repository/todo_repository.dart';
 
-class MainPage extends StatelessWidget {
+import 'todo_list_view.dart';
+
+class MainPage extends StatefulWidget {
   final TodoRepository _repo;
   MainPage(this._repo);
 
   @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<TodoItem>>(
-        future: _repo.getList(), // getList 호출
-        builder: (context, snapshot) {
-          // 수신된 item list
-          List<TodoItem> itemList = snapshot.data;
+      body: Container(
+        color: Colors.grey.shade300,
+        child: FutureBuilder<List<TodoItem>>(
+          future: widget._repo.getList(), // getList 호출
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error'));
+            }
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-          // view
-          return Container();
+            // 수신된 item list
+            List<TodoItem> itemList = snapshot.data;
+            // view
+            return TodoListView(
+              itemList,
+              onTap: (TodoItem item) async {
+                await widget._repo.update(item..isComplete = true);
+                setState(() {});
+              },
+              onLongPress: (TodoItem item) async {
+                await widget._repo.delete(item.id);
+                setState(() {});
+              },
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          String randomTodo = _randomTodo[
+              DateTime.now().millisecondsSinceEpoch % _randomTodo.length];
+          await widget._repo.create(TodoItem.create(randomTodo));
+          setState(() {});
         },
+        child: Icon(Icons.add),
       ),
     );
   }
+
+  List<String> _randomTodo = ['대청소', '빨래', '설거지', '생일 선물 사기', '택배 보내기'];
 }
---
+++ lib/view/todo_list_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:todo/model/todo_item.dart';
+
+class TodoListView extends StatelessWidget {
+  final List<TodoItem> _itemList;
+
+  final Function(TodoItem item) _onTap;
+  final Function(TodoItem item) _onLongPress;
+
+  TodoListView(this._itemList,
+      {Function(TodoItem item) onTap, Function(TodoItem item) onLongPress})
+      : _onTap = onTap,
+        _onLongPress = onLongPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: List.generate(_itemList.length, (idx) {
+        TodoItem item = _itemList[idx];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+          child: Card(
+            color: item.isComplete ? Colors.blue.shade200 : Colors.white,
+            child: InkWell(
+              onTap: () {
+                if (_onTap != null) _onTap(item);
+              },
+              onLongPress: () {
+                if (_onLongPress != null) _onLongPress(item);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(item.display()),
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+    ;
+  }
+}
